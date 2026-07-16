230b10f6acde9e4d4aa15ec31a217045d9e86252
chintireddy <purushotham.chintireddy@gmail.com>
2026-07-16 11:49:48 +0530

    Code pushed to dev post changes. Have added comments in Application.java with regards to context object and its possibilities.

--- mm-getting-started/src/main/java/com/shree/started/Application.java
+++ mm-getting-started/src/main/java/com/shree/started/Application.java
@@ -10,5 +10,12 @@ public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+
+        // Log a message indicating that the application has started
+        logger.info("Application has started successfully.");
+
+        logger.info("Context can be accessed using: ApplicationContext applicationContext = Micronaut.build().start();");
+
+        logger.info("Micronaut.run() method is used to start the application and initialize the application context and returns ApplicationContext as value which is implemented as child class of Interface ApplicationContext.");
     }
 }
